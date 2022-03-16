1292e80acc25241bd2cc7abacc38614ec7a20a3e
cao'jia'jun <2026302174@qq.com>
2022-03-16 15:34:50 +0800

    Changes

---
+++ src/main/java/com/caojiajun/week2/HelloWorldServlet.java
@@ -0,0 +1,17 @@
+package com.caojiajun.week2;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class HelloWorldServlet extends HttpServlet {
+    public void doGet(HttpServletRequest request,HttpServletResponse response) throws IOException {
+        PrintWriter writer=response.getWriter();
+        writer.println("Hello Client !!!");
+    }
+    public void doPost(HttpServletRequest request,HttpServletResponse response){
+
+    }
+}‘
